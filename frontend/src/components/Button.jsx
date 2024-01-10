3ea4adde9a5d63ef295bd0032984da7c8786c2ce
Valeria <lerik13@gmail.com>
2024-01-10 12:15:51 -0700

    update style for button

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({ text, onClickHandler }) => {
 	return (
 		<button
 			onClick={onClickHandler}
-			className="px-12 py-3.5 align-baseline bg-brightRed text-bgColor border rounded-full border-brightRed transition-all duration-500 ease-out"
+			className="btn"
 		>
 			{text}
 		</button>
